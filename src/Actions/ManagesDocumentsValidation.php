d1ec648aa7ee99bc654f39880462651a92c1a8cf
sinnrrr <dimasoltusyuk@gmail.com>
2021-08-11 17:50:35 +0300

    Redo debug

--- src/Actions/ManagesDocumentsValidation.php
+++ src/Actions/ManagesDocumentsValidation.php
@@ -6,8 +6,6 @@ trait ManagesDocumentsValidation
 {
     public function validateDocumentAndName(array $data): bool
     {
-        var_dump($this->post('v1/acquirers/document-identification', $data));
-        die();
         return $this->post('v1/acquirers/document-identification', $data)['success'];
     }
 
