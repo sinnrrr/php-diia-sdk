6f54509257416488efd5e4010c2fa626303bdeee
sinnrrr <dimasoltusyuk@gmail.com>
2021-08-11 17:47:14 +0300

    Validate document debug

--- src/Actions/ManagesDocumentsValidation.php
+++ src/Actions/ManagesDocumentsValidation.php
@@ -6,6 +6,8 @@ trait ManagesDocumentsValidation
 {
     public function validateDocumentAndName(array $data): bool
     {
+        var_dump($this->post('v1/acquirers/document-identification', $data));
+        die();
         return $this->post('v1/acquirers/document-identification', $data)['success'];
     }
 
